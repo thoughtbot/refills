10c0ce2ddcdb0c019f09f3557a6b0b77493f4cdb
Magnus Gyllensward <magnus@thoughtbot.com>
2014-03-17 11:23:59 +0100

    adds automatically updating TOC

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,10 @@
 //= require ZeroClipboard.min
 //= require smooth-scroll
 //= require_tree .
+
+
+$(document).ready(function() {
+    $('#example1').erToc({'goTopNode':'#example1', 'startLevel': 'h2', 'numberedSuffix':'. '});
+    $('#example2').erToc({'goTopNode':'#example2', 'startLevel': 'h2', 'toc<a href="http://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>': false});
+    $('#example3').erToc({'goTopNode':'#example3', 'startLevel': 'h2', 'numbered': false, 'tocClass': 'er_toc er_toc_disc'});
+});
