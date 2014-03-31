45dcdaf4eb7fa26413345f5920999a5f317afdeb
DinoRosic <dino.rosic@me.com>
2014-03-31 16:26:40 +0200

    Use Rails naming conventions for partials

--- lib/refills/import_generator.rb
+++ lib/refills/import_generator.rb
@@ -7,15 +7,41 @@ module Refills
     argument :snippet, type: :string, required: true
 
     def copy_html
-      copy_file "_#{snippet}.html.erb", "app/views/refills/_#{snippet}.html.erb"
+      copy_file view_name(snippet), view_destination
     end
 
     def copy_styles
-      copy_file "stylesheets/refills/_#{snippet}.scss", "app/assets/stylesheets/refills/_#{snippet}.scss"
+      copy_file stylesheet_template, stylesheet_destination
     end
 
     def copy_javascripts
       copy_file "javascripts/refills/_#{snippet}.js", "app/assets/javascripts/refills/_#{snippet}.js"
+    end
+
+    private
+
+    def stylesheet_destination
+      File.join('app', 'assets', 'stylesheets', 'refills', stylesheet_name(snippet_name))
+    end
+
+    def view_destination
+      File.join('app', 'views', 'refills', view_name(snippet_name))
+    end
+
+    def stylesheet_template
+      File.join('stylesheets', 'refills', stylesheet_name(snippet))
+    end
+
+    def view_name(name)
+      "_#{name}.html.erb"
+    end
+
+    def stylesheet_name(name)
+      "_#{name}.scss"
+    end
+
+    def snippet_name
+      snippet.underscore
     end
   end
 end
