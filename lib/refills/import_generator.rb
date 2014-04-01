4adde18503f8050c5e35620393110f50e5e8a892
DinoRosic <dino.rosic@me.com>
2014-04-01 10:34:20 +0200

    refactor import generator

--- lib/refills/import_generator.rb
+++ lib/refills/import_generator.rb
@@ -7,16 +7,40 @@ module Refills
     argument :snippet, type: :string, required: true
 
     def copy_html
-      copy_file "_#{snippet}.html.erb", "app/views/refills/_#{partial_name}.html.erb"
+      copy_file view_template, views_directory
     end
 
     def copy_styles
-      copy_file "stylesheets/refills/_#{snippet}.scss", "app/assets/stylesheets/refills/_#{partial_name}.scss"
+      copy_file stylesheet_template, stylesheets_directory
     end
 
     private
 
-    def partial_name
+    def stylesheets_directory
+      File.join("app", "assets", "stylesheets", "refills", view_name(snippet_name))
+    end
+
+    def views_directory
+      File.join("app", "views", "refills", stylesheet_name(snippet_name))
+    end
+
+    def view_template
+      File.join(view_name(snippet))
+    end
+
+    def stylesheet_template
+      File.join("stylesheets", "refills", stylesheet_name(snippet))
+    end
+
+    def view_name(name)
+      "_#{name}.html.erb"
+    end
+
+    def stylesheet_name(name)
+      "_#{name}.scss"
+    end
+
+    def snippet_name
       snippet.gsub(/-/, '_')
     end
   end
