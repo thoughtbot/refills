c2b85095b310a17fa93de6923158fe11c870837e
Calle Erlandsson <calle@thoughtbot.com>
2014-05-09 16:46:22 +0200

    WIP: Extract all javascript

---
+++ source/javascripts/refills/_accordion.js
@@ -0,0 +1,6 @@
+jQuery('.js-accordion-trigger') // find the trigger
+  .bind('click', function(){
+    jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+    jQuery(this).parent().toggleClass('is-expanded');
+    event.preventDefault();
+});
---
+++ source/javascripts/refills/_sliding-menu.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
