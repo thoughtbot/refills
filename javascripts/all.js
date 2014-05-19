6d5774e1d742d086e21c3aa3e30f1c4915f74544
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-05-19 18:55:29 +0200

    Site updated to 94866dc

--- javascripts/all.js
+++ javascripts/all.js
@@ -1568,6 +1568,143 @@ Prism.languages.scss=Prism.languages.extend("css",{comment:{pattern:/(^|[^\\])(\
   };
 
 })(jQuery, window, document);
+$(document).ready(function () {
+  $('.accordion-tabs-minimal').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs-minimal').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs-minimal')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
+$(document).ready(function () {
+  $('.accordion-tabs').each(function(index) {
+    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+  });
+
+  $('.accordion-tabs').on('click', 'li > a', function(event) {
+    if (!$(this).hasClass('is-active')) {
+      event.preventDefault();
+      var accordionTabs = $(this).closest('.accordion-tabs')
+      accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+      $(this).next().toggleClass('is-open').toggle();
+      accordionTabs.find('.is-active').removeClass('is-active');
+      $(this).addClass('is-active');
+    } else {
+      event.preventDefault();
+    }
+  });
+});
+$('.js-accordion-trigger').bind('click', function(){
+  jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+  jQuery(this).parent().toggleClass('is-expanded');
+  event.preventDefault();
+});
+(function($) {
+  $.fn.clickToggle = function(func1, func2) {
+    var funcs = [func1, func2];
+    this.data('toggleclicked', 0);
+    this.click(function() {
+      var data = $(this).data();
+      var tc = data.toggleclicked;
+      $.proxy(funcs[tc], this)();
+      data.toggleclicked = (tc + 1) % 2;
+    });
+    return this;
+  };
+}(jQuery));
+
+$(document).ready(function () {
+  $('.js-dropdown-link').clickToggle(function() {
+    var menu = $(this).siblings('.js-dropdown-menu');
+    menu.css('top', $(this).outerHeight() + 15);
+    menu.fadeIn(150);
+    $(this).addClass("is-active");
+  }, function() {
+    var menu = $(this).siblings('.dropdown-menu');
+    menu.fadeOut(150);
+    $(this).removeClass("is-active");
+  });
+});
+$(document).ready(function() {
+  var menu = $('#navigation-menu');
+  var menuToggle = $('#js-mobile-menu');
+  var signUp = $('.sign-up');
+
+  $(menuToggle).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle(function(){
+      if(menu.is(':hidden')) {
+        menu.removeAttr('style');
+      }
+    });
+  });
+
+  // underline under the active nav item
+  $(".nav .nav-link").click(function() {
+    $(".nav .nav-link").each(function() {
+      $(this).removeClass("active-nav-item");
+    });
+    $(this).addClass("active-nav-item");
+    $(".nav .more").removeClass("active-nav-item");
+  });
+});
+$(document).ready(function(){
+  $('.js-menu-trigger').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+
+  $('.js-menu-screen').on('click touchstart', function(e){
+    $('.js-menu').toggleClass('is-visible');
+    $('.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
+$(".js-vertical-tab-content").hide();
+$(".js-vertical-tab-content:first").show();
+
+/* if in tab mode */
+$(".js-vertical-tab").click(function(event) {
+  event.preventDefault();
+
+  $(".js-vertical-tab-content").hide();
+  var activeTab = $(this).attr("rel");
+  $("#"+activeTab).show();
+
+  $(".js-vertical-tab").removeClass("is-active");
+  $(this).addClass("is-active");
+
+  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+  $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+});
+
+/* if in accordion mode */
+$(".js-vertical-tab-accordion-heading").click(function(event) {
+  event.preventDefault();
+
+  $(".js-vertical-tab-content").hide();
+  var accordion_activeTab = $(this).attr("rel");
+  $("#"+accordion_activeTab).show();
+
+  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+  $(this).addClass("is-active");
+
+  $(".js-vertical-tab").removeClass("is-active");
+  $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+});
 
 
 
