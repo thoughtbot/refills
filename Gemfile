d7a0b4d2808109c1775c3708b13f664ebee1c9c1
Tyson Gach <tyson@thoughtbot.com>
2014-07-24 09:24:33 -0400

    General cleanup

--- Gemfile
+++ Gemfile
@@ -1,14 +1,11 @@
-# If you have OpenSSL installed, we recommend updating
-# the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.3.3"
-gem 'bourbon', "~>3.2.0"
-gem 'neat', "~> 1.5.0"
-gem 'bitters'
+gem 'middleman', '~>3.3.3'
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
+gem 'bitters'
+gem 'bourbon', '~>3.2.0'
+gem 'middleman-livereload', '~> 3.1.0'
+gem 'neat', '~> 1.5.0'
 
 # For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
+gem 'wdm', '~> 0.1.0', :platforms => [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,63 +1,11 @@
 require 'erubis'
 require 'lib/snippet_helpers'
 
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 activate :directory_indexes
 
-# set :build_dir, "tmp"
-
 set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
-
+set :js_dir, 'javascripts'
 
 helpers SnippetHelpers
 
@@ -66,24 +14,11 @@ ready do
   sprockets.append_path 'vendor/javascripts'
 end
 
-# Build-specific configuration
-configure :build do
+configure :development do
+  activate :livereload
+end
 
+configure :build do
   activate :relative_assets
   set :relative_links, true
-  
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
