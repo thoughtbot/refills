2f21b38d254ddb93ad2c45d3094b2e5cefe336c6
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-07-31 17:14:59 +0200

    Site updated to e543c97

--- javascripts/all.js
+++ javascripts/all.js
@@ -1810,31 +1810,14 @@ $(document).ready(function() {
     });
   });
 });
-(function($) {
-  $.fn.clickToggle = function(func1, func2) {
-    var funcs = [func1, func2];
-    this.data('toggleclicked', 0);
-    this.click(function() {
-      var data = $(this).data();
-      var tc = data.toggleclicked;
-      $.proxy(funcs[tc], this)();
-      data.toggleclicked = (tc + 1) % 2;
+$(document).ready(function(){
+  $(".dropdown-button").click(function(){
+    $(".menu").toggleClass("show-menu");
+    $(".menu > li").click(function(){
+      $(".dropdown-button").html($(this).html());
+      $(".menu").removeClass("show-menu");
     });
-    return this;
-  };
-}(jQuery));
-
-$(document).ready(function () {
-  $('.js-dropdown-link').clickToggle(function() {
-    var menu = $(this).siblings('.js-dropdown-menu');
-    menu.css('top', $(this).outerHeight() + 15);
-    menu.fadeIn(150);
-    $(this).addClass("is-active");
-  }, function() {
-    var menu = $(this).siblings('.dropdown-menu');
-    menu.fadeOut(150);
-    $(this).removeClass("is-active");
-  });
+  });  
 });
 $(document).ready(function() {
   var menu = $('#navigation-menu');
