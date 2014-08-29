7ba802242da4f0a890b2bf58dfe6c5f2d5b01b7d
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-08-29 15:17:40 +0200

    Site updated to 3a18302

--- javascripts/all.js
+++ javascripts/all.js
@@ -2005,8 +2005,6 @@ $(".js-vertical-tab-accordion-heading").click(function(event) {
 
 
 $(document).ready(function() {
-    $('#example1').erToc({'goTopNode':'#example1', 'startLevel': 'h2', 'numberedSuffix':'. '});
-    $('#example2').erToc({'goTopNode':'#example2', 'startLevel': 'h2', 'toc<a href="http://www.jqueryscript.net/tags.php?/Scroll/">Scroll</a>': false});
-    $('#example3').erToc({'goTopNode':'#example3', 'startLevel': 'h2', 'numbered': false, 'tocClass': 'er_toc er_toc_disc'});
+    $('#example').erToc({'goTopNode':'#example', 'startLevel': 'h2', 'numberedSuffix':'. '});
     $( '.refills-menu-anchor' ).fixedsticky();
 });
