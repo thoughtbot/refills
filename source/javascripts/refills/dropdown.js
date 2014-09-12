06af282b9fba41c1f4a4f937cd27f9af88d1cfa7
FabienB4 <fabienb4@users.noreply.github.com>
2014-09-12 10:34:55 +0200

    Converted single quotes to double quotes
    According to https://github.com/thoughtbot/refills/pull/125

--- source/javascripts/refills/dropdown.js
+++ source/javascripts/refills/dropdown.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-  $('.dropdown-button').click(function(){
-    $('.menu').toggleClass('show-menu');
-    $('.menu > li').click(function(){
-      $('.menu').removeClass('show-menu');
+  $(".dropdown-button").click(function(){
+    $(".menu").toggleClass("show-menu");
+    $(".menu > li").click(function(){
+      $(".menu").removeClass("show-menu");
     });
     // simulate a select tag by showing selected value
     // instead of placeholder when a value is selected.
-    $('.menu.select > li').click(function(){
-      $('.dropdown-button').html($(this).html());
+    $(".menu.select > li").click(function(){
+      $(".dropdown-button").html($(this).html());
     });
   });
 });
