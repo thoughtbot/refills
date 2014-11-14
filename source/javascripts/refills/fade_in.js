4dc6e3fe9cc25f6bc9e33c26cfd10f926dbc85f4
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-11-14 14:25:39 +0100

    Add fade in behavior

---
+++ source/javascripts/refills/fade_in.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var element = document.getElementById("js-fadeInElement");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    var elementTopToPageTop = $(element).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var windowInnerHeight = window.innerHeight;
+    var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+    var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+    var distanceFromBottomToAppear = 300;
+
+    if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+      $(element).addClass('js-fade-element-show');
+    }
+    else if(elementTopToWindowBottom < 0) {
+      $(element).removeClass('js-fade-element-show');
+      $(element).addClass('js-fade-element-hide');
+    }
+  });
+});
