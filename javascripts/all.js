78169ff5ad92f26d87158f396500837b4266307c
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-11-14 16:29:17 +0100

    Site updated to 4dc6e3f

--- javascripts/all.js
+++ javascripts/all.js
@@ -1884,6 +1884,27 @@ $(function() {
   return Expander.enhance();
 });
 $(document).ready(function() {
+  var element = document.getElementById("js-fadeInElement");
+  $(element).addClass('js-fade-element-hide');
+
+  $(window).scroll(function() {
+    var elementTopToPageTop = $(element).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var windowInnerHeight = window.innerHeight;
+    var elementTopToWindowTop = elementTopToPageTop - windowTopToPageTop;
+    var elementTopToWindowBottom = windowInnerHeight - elementTopToWindowTop;
+    var distanceFromBottomToAppear = 300;
+
+    if(elementTopToWindowBottom > distanceFromBottomToAppear) {
+      $(element).addClass('js-fade-element-show');
+    }
+    else if(elementTopToWindowBottom < 0) {
+      $(element).removeClass('js-fade-element-show');
+      $(element).addClass('js-fade-element-hide');
+    }
+  });
+});
+$(document).ready(function() {
   var menu = $('#navigation-menu');
   var menuToggle = $('#js-mobile-menu');
   var signUp = $('.sign-up');
