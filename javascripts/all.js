9e00bd8ea2ba2ae7a9db7c6f6c175f1b76271180
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-11-27 14:35:59 +0100

    Site updated to 544a98d

--- javascripts/all.js
+++ javascripts/all.js
@@ -1885,6 +1885,31 @@ $(document).ready(function() {
     $(".nav .more").removeClass("active-nav-item");
   });
 });
+$(document).ready(function() {
+  parallax();
+});
+
+$(window).scroll(function(e) {
+  parallax();
+});
+
+function parallax(){
+  var plxBackground = $("#js-parallax-background");
+  var plxWindow = $("#js-parallax-window");
+
+  var plxWindowTopToPageTop = $(plxWindow).offset().top;
+  var windowTopToPageTop = $(window).scrollTop();
+  var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+  var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+  var windowInnerHeight = window.innerHeight;
+  var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+  var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+  var plxSpeed = 0.35;
+
+  plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+}
+;
 (function (jQuery) {
   jQuery.mark = {
     jump: function (options) {
---
+++ javascripts/refills/parallax.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  parallax();
+});
+
+$(window).scroll(function(e) {
+  parallax();
+});
+
+function parallax(){
+  var plxBackground = $("#js-parallax-background");
+  var plxWindow = $("#js-parallax-window");
+
+  var plxWindowTopToPageTop = $(plxWindow).offset().top;
+  var windowTopToPageTop = $(window).scrollTop();
+  var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+  var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+  var windowInnerHeight = window.innerHeight;
+  var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+  var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+  var plxSpeed = 0.35;
+
+  plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+}
+;
