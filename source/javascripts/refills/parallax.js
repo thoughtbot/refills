4bf9d6ba40b16c673f787f36519fed1cde565b75
Magnus Gyllenswärd <magnus@thoughtbot.com>
2014-12-12 14:18:22 +0100

    gs

--- source/javascripts/refills/parallax.js
+++ source/javascripts/refills/parallax.js
@@ -11,18 +11,20 @@ $(window).scroll(function(e) {
 });
 
 function parallax(){
-  var plxBackground = $("#js-parallax-background");
-  var plxWindow = $("#js-parallax-window");
+  if( $("#js-parallax-window").length > 0 ) {
+    var plxBackground = $("#js-parallax-background");
+    var plxWindow = $("#js-parallax-window");
 
-  var plxWindowTopToPageTop = $(plxWindow).offset().top;
-  var windowTopToPageTop = $(window).scrollTop();
-  var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+    var plxWindowTopToPageTop = $(plxWindow).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
 
-  var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
-  var windowInnerHeight = window.innerHeight;
-  var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
-  var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-  var plxSpeed = 0.35;
+    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    var windowInnerHeight = window.innerHeight;
+    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    var plxSpeed = 0.35;
 
-  plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+  }
 }
