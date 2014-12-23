7af8cdba2524d9ec4e6eb46c9c4d6ed95e04267a
Jesús R Peinado <jesusrpeinado@gmail.com>
2014-12-23 11:48:30 +0100

    Cleaning JS

--- source/javascripts/refills/sliding_menu.js
+++ source/javascripts/refills/sliding_menu.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $('.js-menu-trigger,.js-menu-screen').click(function () {
+  $('.js-menu-trigger,.js-menu-screen').on('click touchstart',function () {
     $('.js-menu,.js-menu-screen').toggleClass('is-visible');
+    e.preventDefault();
   });
 });
