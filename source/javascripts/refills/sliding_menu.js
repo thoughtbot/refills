37b94bbda905c8a8ff534bc0fddebf0c8e1e2d89
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-01-07 16:39:33 +0100

    Simplify JS for sliding menu

--- source/javascripts/refills/sliding_menu.js
+++ source/javascripts/refills/sliding_menu.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
-  $('.js-menu-trigger').on('click touchstart', function(e){
-    $('.js-menu').toggleClass('is-visible');
-    $('.js-menu-screen').toggleClass('is-visible');
-    e.preventDefault();
-  });
-
-  $('.js-menu-screen').on('click touchstart', function(e){
-    $('.js-menu').toggleClass('is-visible');
-    $('.js-menu-screen').toggleClass('is-visible');
+  $('.js-menu-trigger,.js-menu-screen').on('click touchstart',function () {
+    $('.js-menu,.js-menu-screen').toggleClass('is-visible');
     e.preventDefault();
   });
 });
