8bf8eae0bece36ddb89091e5ab32a6d7f9399842
Tyson Gach <tyson@thoughtbot.com>
2015-01-11 23:03:49 -0500

    Restructure project assets
    - Break out vendor stylesheet and javascript assets
    - Update to Normalize.css 3.0.2
    - Fixes #106

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,11 +1,6 @@
-//= require prism
-//= require ZeroClipboard.min
-//= require smooth-scroll
-//= require fixedsticky
 //= require_tree .
 
-
 $(document).ready(function() {
-    $('#example').erToc({'goTopNode':'#example', 'startLevel': 'h2', 'numberedSuffix':'. '});
-    $( '.refills-menu-anchor' ).fixedsticky();
+  $('#example').erToc({'goTopNode':'#example', 'startLevel': 'h2', 'numberedSuffix':'. '});
+  $('.refills-menu-anchor').fixedsticky();
 });
