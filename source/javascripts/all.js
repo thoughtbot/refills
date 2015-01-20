8e8e00c1e45ba28a932ab3e51149c75feb1be20d
Teo Ljungberg <teo@teoljungberg.com>
2015-01-20 21:06:29 +0100

    Exclude coffeescript from being loaded
    `require_tree` requires everything recursively under the given path,
    and we want to ignore `refills/coffeescript` entirely.
    So we pair `require_directory` which does not share that behaviour, with
    requiring the files we need form the `.`.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,8 @@
-//= require_tree .
+//= require jquery.erToc
+//= require jquery.glide
+//= require menuAnchorAnimation
+//= require_directory ./vendor
+//= require_directory ./refills
 
 $(document).ready(function() {
   $('#example').erToc({'goTopNode':'#example', 'startLevel': 'h2', 'numberedSuffix':'. '});
