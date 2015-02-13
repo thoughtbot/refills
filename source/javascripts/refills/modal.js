718612c7648975aa855108b50d2291f83c1dea3d
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-02-13 16:44:28 +0100

    WIP

--- source/javascripts/refills/modal.js
+++ source/javascripts/refills/modal.js
@@ -1,5 +1,5 @@
 $(function() {
-  $("#modal-1").on("click", function() {
+  $("#modal-1").on("change", function() {
     if ($(this).is(":checked")) {
       $("body").addClass("modal-open");
     } else {
@@ -7,7 +7,7 @@ $(function() {
     }
   });
 
-  $(".modal-window").on("click", function() {
+  $(".modal-fade-screen, .modal-close").on("click", function() {
     $(".modal-state:checked").prop("checked", false).change();
   });
 
