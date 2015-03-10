5b0b36b31d75eeccb71fa40c3a32f4cddb604497
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-03-10 11:54:24 +0100

    Add unstyled sliding menu

---
+++ source/javascripts/refills/unstyled/sliding_panel.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.sliding-panel-button,.sliding-panel-fade-screen,.sliding-panel-close').on('click touchstart',function (e) {
+    $('.sliding-panel-content,.sliding-panel-fade-screen').toggleClass('is-visible');
+    e.preventDefault();
+  });
+});
