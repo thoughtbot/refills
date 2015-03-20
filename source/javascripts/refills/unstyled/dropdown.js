7da01532d1976ca45208a220cc52ffdfb1a7b560
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-03-20 15:41:30 +0100

    Add dropdown js

---
+++ source/javascripts/refills/unstyled/dropdown.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".dropdown-button").click(function() {
+    $(".dropdown-menu").toggleClass("show-menu");
+    $(".dropdown-menu > li").click(function(){
+      $(".dropdown-menu").removeClass("show-menu");
+    });
+    $(".dropdown-menu.dropdown-select > li").click(function() {
+      $(".dropdown-button").html($(this).html());
+    });
+  });
+});
+
