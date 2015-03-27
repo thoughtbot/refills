0867e20deab2041131ec4b637f2953b5e80c544c
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-03-27 17:09:49 +0100

    Site updated to 6855c45

--- javascripts/all.js
+++ javascripts/all.js
@@ -1914,6 +1914,19 @@ $(document).ready(function() {
   });
 });
 $(document).ready(function() {
+  var menuToggle = $("#js-navigation-centered-mobile-menu").unbind();
+  $("#js-navigation-centered-menu").removeClass("show");
+  
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-centered-menu").slideToggle(function(){
+      if($("#js-navigation-centered-menu").is(":hidden")) {
+        $("#js-navigation-centered-menu").removeAttr("style");
+      }
+    });
+  });
+});
+$(document).ready(function() {
   if ($("#js-parallax-window").length) {
     parallax();
   }
