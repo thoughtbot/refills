341f891fa554c483ab0f834930d74e7fa0e98b47
Magnus Gyllenswärd <magnus@thoughtbot.com>
2015-09-11 11:32:12 +0200

    Fix second layer MORE not showing when hovering on navigation

--- source/javascripts/refills/navigation.js
+++ source/javascripts/refills/navigation.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+  $('#js-navigation-menu').hide().show(0);
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
