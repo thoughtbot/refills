3d4f3ce7943ab4ee78b735475ce1975f52ac3068
Magnus-G <korrespondera@gmail.com>
2016-01-29 13:39:48 +0000

    Site updated to a0e48b4

--- javascripts/all.js
+++ javascripts/all.js
@@ -1846,10 +1846,7 @@ $(document).ready(function() {
   });
 });
 $(document).ready(function() {
-  var expanderTrigger = document.getElementById("js-expander-trigger");
-  var expanderContent = document.getElementById("js-expander-content");
-
-  $('#js-expander-trigger').click(function(){
+  $('.expander-trigger').click(function(){
     $(this).toggleClass("expander-hidden");
   });
 });
