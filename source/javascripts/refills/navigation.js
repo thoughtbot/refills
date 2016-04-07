0bf747df69bd4c1d5a45776522a4e948bf963db8
Magnus-G <korrespondera@gmail.com>
2016-04-07 16:45:49 +0200

    Make sub-submenu of nav and center nav responsive

--- source/javascripts/refills/navigation.js
+++ source/javascripts/refills/navigation.js
@@ -1,13 +1,30 @@
-$(document).ready(function() {
-  var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+$(window).resize(function() {
+  var more = document.getElementById("js-navigation-more");
+  var windowWidth = $(window).width();
+  var moreLeftSideToPageLeftSide = $(more).offset().left;
+  var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
 
-  menuToggle.on('click', function(e) {
+  if (moreLeftSideToPageRightSide < 330) {
+    $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+    $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+  }
+
+  if (moreLeftSideToPageRightSide > 330) {
+    $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+    $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+  }
+});
+
+$(document).ready(function() {
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
     e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
       }
     });
   });
-});
+}); 
