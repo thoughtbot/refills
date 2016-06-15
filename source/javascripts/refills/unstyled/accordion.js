7bb3233aa9521cba34b6f42c22b98d4de611ce43
Hassen <hassenbtn@gmail.com>
2016-06-16 00:58:58 +0100

    Remove ustyled refills

--- source/javascripts/refills/unstyled/accordion.js
+++
@@ -1,5 +0,0 @@
-$('.js-accordion-trigger').bind('click', function(e){
-  jQuery(this).parent().find('.submenu').slideToggle('fast');
-  jQuery(this).parent().toggleClass('is-expanded');
-  e.preventDefault();
-});
--- source/javascripts/refills/unstyled/expander.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var expanderTrigger = document.getElementById("js-expander-trigger");
-  var expanderContent = document.getElementById("js-expander-content");
-
-  $('#js-expander-trigger').click(function(){
-    $(this).toggleClass("expander-hidden");
-  });
-});
