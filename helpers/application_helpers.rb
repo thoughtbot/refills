7653d3419c8e1f2643de4aeb965886e7f6745671
Tyson Gach <tyson@tysongach.com>
2017-02-04 17:34:54 -0500

    Move inline_svg helper to helpers directory
    Middleman automatically loads helpers placed in this directory.

---
+++ helpers/application_helpers.rb
@@ -0,0 +1,20 @@
+module ApplicationHelpers
+  require "nokogiri"
+
+  def inline_svg(name, options = {})
+    root = Middleman::Application.root
+    file_path = "#{root}/source/images/#{name}.svg"
+
+    if File.exists?(file_path)
+      file = File.read(file_path)
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      if options[:class].present?
+        svg["class"] = options[:class]
+      end
+      svg
+    else
+      "Not found"
+    end
+  end
+end
